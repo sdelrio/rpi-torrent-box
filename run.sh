1b64efb0799408e283bd9610d91d5ed6deb00a61
root <oftheriver@gmail.com>
2015-08-31 00:32:23 +0200

    buid ports and box name

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -dt --name rpi-torrent -p 7080:80 -p 7443:443 -p 7160:49160/udp -p 7161:49161 -v ~/data:/rtorrent rpi-torrent
-
+docker run -dt --name rpi-torrent -p 8080:80 -p 7443:443 -p 49160:49160/udp -p 49161:49161 -v ~/data:/rtorrent rpi-torrent-box
