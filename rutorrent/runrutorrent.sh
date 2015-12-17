3b2561dcd6ef6b3a00ac4f77be7a7e0b0efdd339
Sergio del Rio <oftheriver@gmail.com>
2015-12-17 21:24:30 +0100

    add environment NEW_USER NEW_PASS to choose user/pass when creating container

--- rutorrent/runrutorrent.sh
+++ rutorrent/runrutorrent.sh
@@ -6,11 +6,17 @@ mkdir -p /rtorrent/{downloads,watch,.session,rutorrent/user-profiles,rutorrent/u
 # Change permission to ruTorrent's webserver user
 chown -R www-data:www-data /rtorrent/{downloads,watch,.session,rutorrent/user-profiles,rutorrent/user-profiles/torrents}
 
+PASS_FILE=/rtorrent/.htpasswd
+
+if [ "$NEW_USER" ] && [ "$NEW_PASS" ]; then
+  PASSWORD="$NEW_PASS";SALT="$(openssl rand -base64 3)";SHA1=$(printf "$PASSWORD$SALT" | openssl dgst -binary -sha1 | sed 's#$#'"$SALT"'#' | base64);printf "$NEW_USER:{SSHA}$SHA1\n" > $PASS_FILE
+fi
+
 # Make .htaccess configurable by the user using the volume maped on /rtorrent
-if [ -f /rtorrent/.htpasswd ]; then
-  cp -f /rtorrent/.htpasswd /usr/share/nginx/html/rutorrent/.htpasswd
+if [ -f $PASS_FILE ]; then
+  cp -f $PASS_FILE /usr/share/nginx/html/rutorrent/.htpasswd
 else
-  cp -f /usr/share/nginx/html/rutorrent/.htpasswd /rtorrent/.htpasswd 
+  cp -f /usr/share/nginx/html/rutorrent/.htpasswd $PASS_FILE
 fi
 
 if [ -d /usr/share/nginx/html/rutorrent/conf/ ]; then
