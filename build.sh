dc87ada6716bfce6094859ef11a4f5e1a0d6b43d
Sergio del Rio <oftheriver@gmail.com>
2016-04-13 12:32:46 +0200

    give parameters to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 set -e
 set -x
-docker build -t rpi-rtorrent-box:latest .
+
+APPLICATION=$1
+VERSION=$2
+
+
+if [ -f ./Dockerfile ]; then
+ docker_dir=./
+fi
+
+if [ -z $VERSION ]; then
+  VERSION=latest
+i
+
+if [ -z $APPLICATION ]; then
+  APPLICATION=rpi-rtorrent-box
+fi 
+
+docker build -t $APPLICATION:$VERSION $docker_dir
