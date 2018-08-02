060880ac30dff2ab08b2d53a16f5461beb9fea3e
Sergio del Rio <oftheriver@gmail.com>
2018-08-02 19:40:05 +0200

    Set image env var for arm build scripts.

--- arm_curl.sh
+++ arm_curl.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if docker pull sdelrio/arm-curl-copy ; then
+IMAGE=sdelrio/arm-curl-copy
+if docker pull $IMAGE ; then
     echo - Getting arm curl from previous build
 else
 
     mkdir build/curl-7.39.0
     make curl PACK_IMAGENAME=sdelrio/rpi-torrent-box BUILDER_BASE=resin/rpi-raspbian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder
-    docker build -t sdelrio/arm-curl-copy . -f -<<EOF
+    docker build -t $IMAGE . -f -<<EOF
 FROM busybox
 COPY ./build/curl-7.39.0 /copy
 EOF
 
-    docker push sdelrio/arm-curl-copy
     echo - Pushing arm curl from next builds
+    docker push $IMAGE
 fi
 
--- arm_xmlrpc.sh
+++ arm_xmlrpc.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-if docker pull sdelrio/arm-xmlrpc-copy ; then
+IMAGE=sdelrio/arm-xmlrpc-copy
+
+if docker pull $IMAGE ; then
     echo - Getting arm xmlrpc from previous build
 else
 
     mkdir build/xmlrpc-c
     make xmlrpc PACK_IMAGENAME=sdelrio/rpi-torrent-box BUILDER_BASE=resin/rpi-raspbian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder
-    docker build -t sdelrio/arm-xmlprc-copy . -f -<<EOF
+    docker build -t $IMAGE . -f -<<EOF
 FROM busybox
 COPY ./build/xmlrpc /copy
 EOF
 
-    docker push sdelrio/arm-xmlrpc-copy
     echo - Pushing arm xmlrpc from next builds
+    docker push $IMAGE
 fi
 
