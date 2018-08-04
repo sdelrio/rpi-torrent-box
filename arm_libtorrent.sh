5ce5e99f05c10c4fe3f258dcc325f89c814ca06d
Sergio del Rio <oftheriver@gmail.com>
2018-08-04 11:30:22 +0200

    add arm libtorrent compilation.

---
+++ arm_libtorrent.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE=sdelrio/arm-libtorrent-copy
+
+if docker pull $IMAGE ; then
+    echo - Getting arm libtorrent from previous build
+else
+
+    mkdir libtorrent-$VER_LIBTORRENT
+    make libtorrent PACK_IMAGENAME=sdelrio/rpi-torrent-box BUILDER_BASE=resin/rpi-raspbian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder
+    docker build -t $IMAGE . -f -<<EOF
+FROM busybox
+COPY ./build/libtorrent_$VER_LIBTORRENT /copy
+EOF
+
+    echo - Pushing arm libtorrent from next builds
+    docker push $IMAGE
+fi
+
