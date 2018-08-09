598a191ada597c120c1d5caab1cb7ac55691548a
Sergio del Rio <oftheriver@gmail.com>
2018-08-09 17:51:07 +0200

    add rtorrent build for arm using travis

--- arm_rtorrent.sh
+++ arm_rtorrent.sh
@@ -3,14 +3,14 @@
 IMAGE=sdelrio/arm-rtorrent-copy
 
 if docker pull $IMAGE ; then
-    echo - Getting arm libtorrent from previous build
+    echo - Getting arm rtorrent from previous build
 else
 
     mkdir rtorrent-$VER_RTORRENT
     make rtorrent PACK_IMAGENAME=sdelrio/rpi-torrent-box BUILDER_BASE=resin/rpi-raspbian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder
     docker build -t $IMAGE . -f -<<EOF
 FROM busybox
-COPY ./build/rtorrent-$VER_RTORRENT /copy
+COPY ./build/rtorrent-$VER_LIBTORRENT /copy
 EOF
 
     echo - Pushing arm libtorrent from next builds
