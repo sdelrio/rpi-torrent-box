8898922680934a0a80c64898302b0744c7f57ef0
Sergio del Rio <oftheriver@gmail.com>
2018-08-12 00:38:14 +0200

    update arm libtorrent compilation

--- arm_libtorrent.sh
+++ arm_libtorrent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 IMAGE=sdelrio/arm-libtorrent-copy
 
--- compile_libtorrent.sh
+++ compile_libtorrent.sh
@@ -6,7 +6,7 @@ cd libtorrent-$VER_LIBTORRENT
 echo - Autogent compile libtorrent-$VER_LIBTORRENT
 ./autogen.sh
 echo - Configure compile libtorrent-$VER_LIBTORRENT
-./configure --quiet --enable-silent-rules --with-posix-fallocate
+./configure --quiet --enable-glibc-back-compat --enable-silent-rules --with-posix-fallocate
 make V=0
 echo - Finish compile libtorrent-$VER_LIBTORRENT
 
