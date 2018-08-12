b5194e5955bf15ad4bd2e65a2e686082c7658c5f
Sergio del Rio <oftheriver@gmail.com>
2018-08-12 13:52:38 +0200

    Fix curl so names for curl 7.39.0. Fix env var.

--- compile_curl.sh
+++ compile_curl.sh
@@ -11,7 +11,7 @@ export CXXFLAGS="${CFLAGS}"
 ./configure --quiet --enable-silent-rules --enable-ares --enable-tls-srp --enable-gnu-tls --with-zlib --with-ssl
 echo - Begin compiling curl
 make V=0
-
+make install
 ls -la /usr/local/lib
 ls -la /usr/local/bin
 
