999839ac3f787b8cbe3fa4dd6ba3f7ee8d8514d2
Sergio R <oftheriver@gmail.com>
2019-07-23 22:38:10 +0200

    update rpi2 images on builders. add path to php-fpm

--- make_builder_arm.sh
+++ make_builder_arm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make BUILDER_BASE=resin/rpi-raspbian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder builder
+make BUILDER_BASE=balenalib/raspberry-pi2-debian:jessie GCCBUILDER_IMAGENAME=sdelrio/rpi-gccbuilder builder
 
